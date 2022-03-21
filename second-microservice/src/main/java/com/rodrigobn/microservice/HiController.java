9b7b7a272ff6204db5c4bbfc51ea2eb8a05a94b5
Rodrigo Brito <rodrito90@gmail.com>
2022-03-21 16:10:37 -0300

    Subindo os dois primeiros microservice
    - Criação do Service Registry

---
+++ second-microservice/src/main/java/com/rodrigobn/microservice/HiController.java
@@ -0,0 +1,16 @@
+package com.rodrigobn.microservice;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hi")
+public class HiController {
+
+    @GetMapping
+    public String getMessage() {
+        return "Hi!";
+    }
+}
